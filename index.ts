649924de8fdc1edd6c99558256928194151fac56
TomasC <tomasleoncanocano@gmail.com>
2026-01-20 11:30:22 +0100

    Añadimos endpoint GET /movies

--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
 import { initDB } from "./db/init"; 
 import { getAllMovies, getMovieById , getMovieByTitle} from "./models/movies";
+import express from "express"
 
-
+const app = express();
+const PORT = 3000
 const db = await initDB()
-//const movies = getAllMovies(db)
-//console.log(movies)
-//const peliculas = getMovieById(db, 1)
-//console.log(peliculas)
-const peliculas = getMovieByTitle(db, "Toy ")
-console.log(peliculas)
+
+app.get("/movies", (req, res )=>{
+    const peliculas = getAllMovies(db)
+    res.json(peliculas)
+})
+
+app.listen(PORT, () =>{
+    console.log(`Servidor funcionando en el puerto ${PORT}`)
+})
