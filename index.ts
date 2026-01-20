26a1dbd1450efadf695ce3882f10a501e9e82636
TomasC <tomasleoncanocano@gmail.com>
2026-01-20 11:17:46 +0100

    Creamos las funciones getMovieById y getMovieByTitle

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import { initDB } from "./db/init"; 
-import { getAllMovies } from "./models/movies";
+import { getAllMovies, getMovieById , getMovieByTitle} from "./models/movies";
+
 
 const db = await initDB()
-const movies = getAllMovies(db)
-console.log(movies)
+//const movies = getAllMovies(db)
+//console.log(movies)
+//const peliculas = getMovieById(db, 1)
+//console.log(peliculas)
+const peliculas = getMovieByTitle(db, "Toy ")
+console.log(peliculas)
--- models/movies.ts
+++ models/movies.ts
@@ -1,8 +1,17 @@
 import { Database } from "bun:sqlite"
 
- export function getAllMovies(db: Database) {
+export function getAllMovies(db: Database) {
     const query = db.query("SELECT * FROM movies")
     return query.all()
 }
 // -obtener peliculas por id
+export function getMovieById(db: Database, id: number) {
+    const query = db.query("SELECT * FROM movies WHERE id = ?")
+    return query.get(id)
+}
 // -obtener peliculas por titulo
+
+export function getMovieByTitle(db: Database, title: string){
+    const query = db.query("SELECT * FROM movies WHERE title LIKE ?")
+    return query.all(`%${title}%`)
+}
