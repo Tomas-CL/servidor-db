7ede9d30eb57f6a0fe54386bf446b7d83c6bb23d
TomasC <tomasleoncanocano@gmail.com>
2026-01-22 10:18:14 +0100

    get /movies ahora acepta parametros proporcionados por el usuario

--- index.ts
+++ index.ts
@@ -1,16 +1,34 @@
-import { initDB } from "./db/init"; 
-import { getAllMovies, getMovieById , getMovieByTitle} from "./models/movies";
+import { initDB } from "./db/init";
+import { getAllMovies, getMovieById, getMovieByTitle } from "./models/movies";
 import express from "express"
+import type { Request, Response, NextFunction } from "express";
 
 const app = express();
 const PORT = 3000
 const db = await initDB()
 
-app.get("/movies", (req, res )=>{
-    const peliculas = getAllMovies(db)
-    res.json(peliculas)
+const logMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.method, req.url, new Date().toDateString())
+    if (req.method === "GET") {
+        console.log(req.query)
+    }
+    next()
+}
+
+app.use(logMiddleware)
+
+app.get("/movies", (req: Request, res: Response) => {
+    const title = req.query.title
+    if (title) {
+        const peliculas = getMovieByTitle(db, title)
+        res.json(peliculas)
+    } else {
+        const peliculas = getAllMovies(db)
+        res.json(peliculas)
+
+    }
 })
 
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log(`Servidor funcionando en el puerto ${PORT}`)
 })
