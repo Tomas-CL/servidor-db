cc69cfb5b2dda757e349d226cf6a40a2cff1feb7
TomasC <tomasleoncano@gmail.com>
2026-02-22 19:33:33 +0100

    crear un archivo yml con la imagen de redis y hacer la cache de GET /movies

--- index.ts
+++ index.ts
@@ -2,32 +2,48 @@ import { initDB } from "./db/init";
 import { getMovies, insertMovies } from "./models/movies";
 import express from "express"
 import type { Request, Response, NextFunction } from "express";
+import { redis } from "bun";
 
 const app = express();
 const PORT = 3000
+
 const db = await initDB()
+
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
 
 const logMiddleware = (req: Request, res: Response, next: NextFunction) => {
     console.log(req.method, req.url, new Date().toDateString())
     if (req.method === "GET") {
         console.log(req.query)
-    }if(req.method === "POST") {
+    } if (req.method === "POST") {
         console.log(req.query)
     }
     next()
 }
 
-app.use(express.static("public"))
 app.use(logMiddleware)
 
-app.get("/movies", (req: Request, res: Response) => {
+// --------------GET /movies
+app.get("/movies",  async (req: Request, res: Response) => {
     const { title, genres } = req.query
     const filters = {
         title: typeof title === "string" ? title : undefined,
         genres: typeof genres === "string" ? genres : undefined
     }
+
+    const cacheKey = `movies:${JSON.stringify(filters)}`
+    const cached = await redis.get(cacheKey)
+    if (cached) {
+        console.log("Respuesta cacheada, puede tener datos desactualizados")
+        return res.json(JSON.parse(cached))
+    }
+
+    console.log("Refrescando cache")
     const movies = getMovies(db, filters)
+
+    await redis.set(cacheKey, JSON.stringify(movies), "EX", 30)
     res.json(movies)
 })
 
